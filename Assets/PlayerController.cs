e487cda3e529e9076be388417231be72eecf5d4b
oskdas <96323392+oskdas@users.noreply.github.com>
2022-02-05 10:58:35 +0800

    コインカウント

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public Text coin;
+    public int score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        coin.text = score.ToString("00");
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Coin")
         {
             Destroy(other.gameObject);
+            score += 1;
         }
     }
 }
